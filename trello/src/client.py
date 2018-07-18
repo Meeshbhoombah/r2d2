ff993231a2e613f15ce259c1b195f73bdf0e7ae0
Meeshbhoombah <rohanmishra2017@gmail.com>
2018-07-17 22:13:05 -0700

    Added README template

--- trello/src/client.py
+++ trello/src/client.py
@@ -6,5 +6,21 @@ Uses `py-trello` to manipulate boards, lists, and cards on the Meeshbhoombah
 Trello
 """
 
+import os
+from pprint import pprint
 
+
+from trello import TrelloClient
+
+
+client = TrelloClient(
+    api_key= os.environ.get('TRELLO_API_KEY'),
+    api_secret=os.environ.get('TRELLO_SECERET_KEY'),
+    token=os.environ.get('TRELLO_TOKEN'),
+    token_secret=os.environ.get('TRELLO_SCECRET_TOKEN')
+)
+
+
+# move all ideas to idea board
+pprint(client.list_boards())
 

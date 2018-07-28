4c0a98d6ce0d1f31e0661e2acdf790528f8ebbc4
Meeshbhoombah <rohanmishra2017@gmail.com>
2018-07-28 15:44:23 -0700

    Added bulk_move to repo

--- trello/src/bulk_move.py
+++ trello/src/bulk_move.py
@@ -1,9 +1,9 @@
 # -*- encoding: utf-8 -*-
 
-"""client.py
+"""/r2d2/trello/src/bulk_move.py
 
 Uses `py-trello` to manipulate boards, lists, and cards on the Meeshbhoombah
-Trello
+Trello.
 """
 
 import os
@@ -13,14 +13,19 @@ from pprint import pprint
 from trello import TrelloClient
 
 
-client = TrelloClient(
-    api_key= os.environ.get('TRELLO_API_KEY'),
-    api_secret=os.environ.get('TRELLO_SECERET_KEY'),
-    token=os.environ.get('TRELLO_TOKEN'),
-    token_secret=os.environ.get('TRELLO_SCECRET_TOKEN')
-)
+def load_trello_details(self, custom_dict):
+    """ Loads in Trello keys and tokens from the environment
+
+    Loads in environment vars contained in `/trello/src/.secret`
+    """
+    client = TrelloClient(
+        api_key= os.environ.get('TRELLO_API_KEY'),
+        api_secret=os.environ.get('TRELLO_SECERET_KEY'),
+        token=os.environ.get('TRELLO_TOKEN'),
+        token_secret=os.environ.get('TRELLO_SCECRET_TOKEN')
+    )
 
 
-# move all ideas to idea board
-pprint(client.list_boards())
+if __name__ == "__main__":
+    pprint(client.list_boards())
 

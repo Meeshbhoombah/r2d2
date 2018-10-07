7638a3e8dcafa0336c0fe12d48f8b6db4f5cc333
Meeshbhoombah <rohanmishra2017@gmail.com>
2018-10-06 20:59:13 -0700

    Added Private Network ports to config

--- config.py
+++ config.py
@@ -2,3 +2,8 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    # Private Network Ports
+    NODE_ONE_PORT = 4201
+    NODE_ONE_PORT = 4202
+

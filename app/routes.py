5d799e672331d879c60ad1bd52e1c0b81a387b1d
Tia King <tiajaneeking@gmail.com>
2018-10-07 00:46:33 -0700

    adding css & bootstrap

--- app/routes.py
+++ app/routes.py
@@ -15,7 +15,7 @@ def login():
 	form = LoginForm()
 	if form.validate_on_submit():
 		flash('Login requested for user {}'.format(form.username.data))
-		return redirect(url_for(index))
+		return redirect(url_for(join))
 	return render_template('login.html', title="log in", form=form)
 
 
@@ -23,6 +23,18 @@ def login():
 def join():
 	return render_template('join.html', title="join")
 
-@app.route('/settings' methods=['GET'])
+@app.route('/settings', methods=['GET'])
 def settings():
 	return render_template('settings.html', title="settings")
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+	return render_template('home.html', title="home")
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+	return render_template('create.html', title="create room")
+
+@app.route('/room_code', methods=['GET'])
+def room_code():
+	return render_template('room_code.html', title="room code")

ba01ec38c543133711e399c2f38c7bc697979b0f
Meeshbhoombah <rohanmishra2017@gmail.com>
2018-10-07 12:49:20 -0700

    Connected to Network

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,13 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
+
+# check if connected to Infura node
+from web3.auto.infura import w3
+assert w3.isConnected(), 'Unable to connect to Infura.'
+
+from web3 import Web3
+web3 = Web3(Web3.HTTPProvider('https://ropsten.infura.io/v3/' + app.config['INFURA_API_KEY']))
+
 from app import routes
+
--- config.py
+++ config.py
@@ -7,3 +7,7 @@ class Config(object):
     NODE_ONE_PORT = 4201
     NODE_ONE_PORT = 4202
 
+    INFURA_API_KEY = os.environ.get('INFURA_API_KEY')
+    PUB_KEY = os.environ.get('PUB_KEY')
+    PRIV_KEY = os.environ.get('PRIV_KEY')
+

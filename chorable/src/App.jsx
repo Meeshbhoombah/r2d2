275e15708c6f59135fbc92d239f6c0a1d2a7822a
Meeshbhoombah <rohanmishra2017@gmail.com>
2018-10-18 23:46:10 -0700

    Added chorable README

---
+++ chorable/src/App.jsx
@@ -0,0 +1,15 @@
+/*
+ * index.js
+ * Entrypoint for the `Chorable` web app
+ *
+ */ 
+
+
+import React     from 'react'
+,      ReactDOM  from 'react-dom'
+,      App       from './App';
+
+// import global css styles to render on #root
+
+
+
